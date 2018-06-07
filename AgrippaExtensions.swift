eaac40fce932e9aa2ccd5495cfb101ecd68a8ac4
Elliott <itali43@gmail.com>
2018-06-07 11:25:22 -0700

    init

---
+++ AgrippaExtensions.swift
@@ -0,0 +1,163 @@
+//
+//  AgrippaExtensions.swift
+//
+//  Created by Elliott Williams on 4/7/18.
+//  Copyright © 2018 AgrippaApps. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// Defaults Project wide
+let defaults = UserDefaults.standard
+
+// button outline extension
+@IBDesignable extension UIButton {
+    
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
+
+// image
+// button outline extension
+@IBDesignable extension UIImageView {
+    
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
+
+// String Extensions
+// for hexadecimal
+extension String {
+    
+    init?(hexadecimal string: String, encoding: String.Encoding = .utf8) {
+        guard let data = string.hexadecimal() else {
+            return nil
+        }
+        
+        self.init(data: data, encoding: encoding)
+    }
+    
+    func hexadecimalString(encoding: String.Encoding = .utf8) -> String? {
+        return data(using: encoding)?
+            .hexadecimal()
+    }
+    
+    /// Create `Data` from hexadecimal string representation
+    ///
+    /// This takes a hexadecimal representation and creates a `Data` object. Note, if the string has any spaces or non-hex characters (e.g. starts with '<' and with a '>'), those are ignored and only hex characters are processed.
+    ///
+    /// - returns: Data represented by this hexadecimal string.
+    
+    func hexadecimal() -> Data? {
+        var data = Data(capacity: characters.count / 2)
+        
+        let regex = try! NSRegularExpression(pattern: "[0-9a-f]{1,2}", options: .caseInsensitive)
+        regex.enumerateMatches(in: self, range: NSMakeRange(0, utf16.count)) { match, flags, stop in
+            let byteString = (self as NSString).substring(with: match!.range)
+            var num = UInt8(byteString, radix: 16)!
+            data.append(&num, count: 1)
+        }
+        guard data.count > 0 else { return nil }
+        return data
+    }
+    
+}
+
+
+
+// Data Extensions
+// for hexadecimal
+extension Data {
+    /// Create hexadecimal string representation of `Data` object.
+    ///
+    /// - returns: `String` representation of this `Data` object.
+    func hexadecimal() -> String {
+        return map { String(format: "%02x", $0) }
+            .joined(separator: "")
+    }
+}
+
+extension UIView {
+    
+    func startRotating(duration: CFTimeInterval = 3, repeatCount: Float = Float.infinity, clockwise: Bool = true) {
+        
+        if self.layer.animation(forKey: "transform.rotation.z") != nil {
+            return
+        }
+        
+        let animation = CABasicAnimation(keyPath: "transform.rotation.z")
+        let direction = clockwise ? 1.0 : -1.0
+        animation.toValue = NSNumber(value: .pi * 2 * direction)
+        animation.duration = duration
+        animation.isCumulative = true
+        animation.repeatCount = repeatCount
+        self.layer.add(animation, forKey:"transform.rotation.z")
+    }
+    
+    func stopRotating() {
+        
+        self.layer.removeAnimation(forKey: "transform.rotation.z")
+        
+    }
+    
+}
+
+
+
+
+
+
+
